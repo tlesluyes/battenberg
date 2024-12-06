81e6e6c22341597f3a949729ed25491d1d3ea77f
Tom Lesluyes <t.lesluyes@gmail.com>
2024-12-06 08:44:52 +0100

    Fix so the gsub doesn't mess up paths with an underscore

--- R/fitcopynumber.R
+++ R/fitcopynumber.R
@@ -356,7 +356,7 @@ callSubclones = function(sample.name, baf.segmented.file, logr.file, rho.psi.fil
   
   # Create user friendly cellularity and ploidy output file
   cellularity_ploidy_output = data.frame(purity = c(rho), ploidy = c(ploidy), psi = c(psit))
-  cellularity_file = gsub("_.+\\.txt$", "_purity_ploidy.txt", output.file) # NAP: updated the name of the output file, consistent with new title (and added flexibility with what output.file is named)
+  cellularity_file = gsub("\\.txt$", "_purity_ploidy.txt", output.file) # NAP: updated the name of the output file, consistent with new title (and added flexibility with what output.file is named)
   write.table(cellularity_ploidy_output, cellularity_file, quote=F, sep="\t", row.names=F)
 }
 
